b0013152812430761737529c7f2dbf529bfe4b04
Sjoerd <spanggas@gmail.com>
2018-09-10 23:15:14 +0200

    Removed dependency to puppeteer and replaced it with request

--- schedule.js
+++ schedule.js
@@ -1,41 +1,23 @@
 var fs = require('fs');
-const puppeteer = require('puppeteer');
 const loginUrl = "https://connect.fontys.nl/my.policy";
+const baseUrl = "https://connect.fontys.nl/";
 const scheduleUrl = "https://www.fontys.nl/roosters/FHTenL-I/37/c/c00003.htm";
 const SCHEDULE_CREDENTIALS_PATH = "fontys.json";
+var request = require('request-promise');
 var credentials = JSON.parse(fs.readFileSync(SCHEDULE_CREDENTIALS_PATH, 'utf8'));
 var screenshot = "login.png";
 
-async function GetScheduleData() {
-	const browser = await puppeteer.launch({headless: false});	
-	await LoginToService(browser);
-	await ScrapeSchedule(browser);
-	browser.close();
+function GetScheduleData() {
+	LoginToService();
 }
 
-async function LoginToService(browser) { 
-	  	const page = await browser.newPage()
-	  	await page.goto(loginUrl)
-	  	await page.click('.btn-primary')
-	  	await page.waitForNavigation()
-	  	await page.waitFor(2000);
-  	  	await page.type('#username', credentials.username, {delay: 100})
-		await page.type('#password', credentials.password, {delay: 100})
-		await page.click('[type="submit"]')
-		await page.waitForNavigation()
-	};
-async function ScrapeSchedule(browser) {
-	const page = await browser.newPage()
-  	await page.goto(scheduleUrl);
-	  const data = await page.evaluate(() => {
-	    const firstRow = document.getElementsByTagName("table")[0].rows[0].cells;
-	    var firstRowData = [];
-	    for (var i = 0; i < firstRow.length; i++) {
-	    	firstRowData.push(firstRow[i].innerText);
-	    }
-	    console.log(firstRowData);
-	    return tds.map(td => td.innerHTML)
-	  });
-	  	console.log(data);
-}
+function LoginToService(browser) { 
+	var payload = {'username': credentials.username,'password': credentials.password, 'vhost': 'standard'}
+	request.get(baseUrl).then(function(){
+		return request.post({
+			headers: {'content-type' : 'application/x-www-form-urlencoded'},
+			url: loginUrl
+		});
+	}).catch(err => console.log);
+};
 module.exports = {"GetScheduleData": GetScheduleData, "SCHEDULE_CREDENTIALS_PATH": SCHEDULE_CREDENTIALS_PATH};
