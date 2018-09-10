69ccb1c9b9924ac56e6b7ad7d38cbc188705c1fd
Sjoerd <spanggas@gmail.com>
2018-09-10 11:33:30 +0200

    Added checkCredentialsExistance function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
-const schedule = require('schedule.js');
+const schedule = require('./schedule.js');
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 const TOKEN_PATH = 'token.json';
@@ -11,8 +11,30 @@ fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), listEvents);
-  schedule.login();
+  checkCredentialExistance(function(err) {
+  	if(err) throw err;
+  });
+
 });
+
+
+/**
+ * Checks if there is a credential JSON file available
+ * @param  {Function} callback the callback with an error if there is one
+ */
+function checkCredentialExistance(callback) {
+	var error = null;
+	if(!fs.existsSync(schedule.SCHEDULE_CREDENTIALS_PATH)) {
+		//Config file doesn't exist, create it
+		console.log("Creating credentials file for login to schedule");
+		var obj = {"username": "", "password": ""};
+		var jsonvalue = JSON.stringify(obj);
+		fs.appendFile(schedule.SCHEDULE_CREDENTIALS_PATH, jsonvalue, function(err) {
+			if(err) error = err;
+		});
+	}
+	callback(error)
+}
 
 
 /**
@@ -70,24 +92,25 @@ function getAccessToken(oAuth2Client, callback) {
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
+  // const calendar = google.calendar({version: 'v3', auth});
+  // calendar.events.list({
+  //   calendarId: 'primary',
+  //   timeMin: (new Date()).toISOString(),
+  //   maxResults: 10,
+  //   singleEvents: true,
+  //   orderBy: 'startTime',
+  // }, (err, res) => {
+  //   if (err) return console.log('The API returned an error: ' + err);
+  //   const events = res.data.items;
+  //   if (events.length) {
+  //     console.log('Upcoming 10 events:');
+  //     events.map((event, i) => {
+  //       const start = event.start.dateTime || event.start.date;
+  //       console.log(`${start} - ${event.summary}`);
+  //     });
+  //   } else {
+  //     console.log('No upcoming events found.');
+  //   }
+  // });
+  schedule.GetScheduleData();
 }
