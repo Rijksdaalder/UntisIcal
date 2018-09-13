9c54ff63afb5a0e350a431fcabf44e09abe7354c
Sjoerd <spanggas@gmail.com>
2018-09-13 16:27:51 +0200

    Started working on the OOP implementation

--- schedule.js
+++ schedule.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var request = require('request');
 const SCHEDULE_CREDENTIALS_PATH = "fontys.json";
-var credentials = JSON.parse(fs.readFileSync(SCHEDULE_CREDENTIALS_PATH, 'utf8'));
 //Related to table parsing
 const loginUrl = "https://connect.fontys.nl/my.policy";
 const baseUrl = "https://connect.fontys.nl/";
@@ -9,17 +8,35 @@ const scheduleUrl = "https://www.fontys.nl/roosters/FHTenL-I/37/c/c00003.htm";
 
 //Related to the JSON API
 const apiUrl = "https://api.fontys.nl/json.ashx?app=M50h7mWVl0i46ZPk0Fftug&iPlannerRooster_action=lessen";
-const instituteId = 43; //43 = venlo
-const classId = "TIPA"; //
+
+
+class ScheduleApi {
+
+	constructor(ShouldGetLoginCookies = false) {
+		//Get the credentials
+		this.credentials = JSON.parse(fs.readFileSync(SCHEDULE_CREDENTIALS_PATH, 'utf8'));
+		this.instituteId = 43; //43 = Fontys Venlo
+		this.className = "TIPA";
+		if(ShouldGetLoginCookies) {
+			loginToService();
+		}
+
+	}
+	loginToService(callback) {
+		var payload = {'username': credentials.username,'password': credentials.password, 'vhost': 'standard'}
+		request.get(baseUrl).then(function() {
+			return request.post({
+				headers: {'content-type' : 'application/x-www-form-urlencoded'},
+				url: loginUrl
+			});
+		}).catch(err => console.log);
+		return callback(null, null) //TODO: implement actual response headers
+	}
+
+}
 
 function LoginToService(browser) { 
-	var payload = {'username': credentials.username,'password': credentials.password, 'vhost': 'standard'}
-	request.get(baseUrl).then(function() {
-		return request.post({
-			headers: {'content-type' : 'application/x-www-form-urlencoded'},
-			url: loginUrl
-		});
-	}).catch(err => console.log);
+	
 };
 function GetScheduleData(callback) {
 	//&iPlannerRooster_instituut=43&iPlannerRooster_klas=TIPA
